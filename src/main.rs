ecbb0f8e4324ebee9112f2f339d3b2570fd113d6
BlakeIngle <blake.ingle7@gmail.com>
2024-05-23 18:55:23 -0400

    undo on a lot of animation stuff

--- src/main.rs
+++ src/main.rs
@@ -49,110 +49,6 @@ struct SpriteAnimation {
     frames: Vec<Sprite>,
 }
 
-impl SpriteAnimation {
-    fn new() -> Self {
-        SpriteAnimation {
-            name: "".to_owned(),
-            frames: Vec::new(),
-        }
-    }
-
-    fn name(mut self, name: &str) -> Self {
-        self.name = name.to_owned();
-        self
-    }
-
-    fn add_frame(mut self, spritesheet: usize, region: Rect) -> Self {
-        self.frames.push(Sprite {
-            spritesheet,
-            region,
-        });
-        self
-    }
-}
-
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct AnimationPlayer {
-    ellapsed_time: f32,
-    animation_duration: f32,
-    playback_speed: f32,
-    paused: bool,
-}
-
-impl AnimationPlayer {
-    fn new(animation_duration: f32) -> Self {
-        AnimationPlayer {
-            animation_duration,
-            ellapsed_time: 0.0,
-            playback_speed: 1.0,
-            paused: false,
-        }
-    }
-
-    fn set_duration(mut self, animation_duration: f32) -> Self {
-        self.animation_duration = animation_duration;
-        self
-    }
-
-    fn set_speed(&mut self, new_speed: f32) {
-        self.playback_speed = new_speed;
-    }
-
-    fn step(&mut self, delta_time: f32) {
-        match self.paused {
-            false => {
-                self.ellapsed_time = (self.ellapsed_time + (delta_time * self.playback_speed))
-                    % self.animation_duration;
-            }
-            true => {}
-        }
-    }
-
-    fn pause(&mut self) {
-        self.paused = true;
-    }
-
-    fn play(&mut self) {
-        self.paused = false;
-    }
-
-    fn stop(&mut self) {
-        self.paused = true;
-        self.ellapsed_time = 0.0;
-    }
-
-    fn current_frame(self) -> u32 {
-        (self.animation_duration / self.ellapsed_time) as u32
-    }
-}
-
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct AnimatedSprite {
-    animation_player: AnimationPlayer,
-    animations: Vec<SpriteAnimation>,
-}
-
-impl AnimatedSprite {
-    fn new() -> Self {
-        AnimatedSprite {
-            animation_player: AnimationPlayer::new(1.0),
-            animations: Vec::new(),
-        }
-    }
-
-    fn set_duration(mut self, animation_duration: f32) -> Self {
-        self.animation_player.set_speed(animation_duration);
-        self
-    }
-
-    fn add_animation(mut self, new_animation: SpriteAnimation) -> Self {
-        self.animations.push(new_animation);
-        self
-    }
-}
-
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -194,12 +90,8 @@ fn main() -> Result<(), String> {
         facing: Direction::Down,
     };
 
-    let idle_animation = SpriteAnimation::new().name("idle");
+    // let idle_animation = SpriteAnimation::new().name("idle");
     // .add_frame(usize::from(texture), Rect::new(0, 0, 26, 36));
-
-    let animated_sprite = AnimatedSprite::new()
-        .set_duration(2.0)
-        .add_animation(idle_animation);
 
     'running: loop {
         canvas.set_draw_color(Color::RGB(255, 0, 255));
